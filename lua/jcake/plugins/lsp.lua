73cafdee0cab726f54a0e2eafa72be6b2bd4fc61
Jon Stockton <jonstockton1416@gmail.com>
2024-01-23 07:38:14 -0500

    Tune LSP diagnostics a bit

--- lua/jcake/plugins/lsp.lua
+++ lua/jcake/plugins/lsp.lua
@@ -23,11 +23,33 @@ return {
         "neovim/nvim-lspconfig",
         config=function()
             local lspconfig = require('lspconfig')
-            lspconfig.lua_ls.setup({})
+
+            -- Lua
+            lspconfig.lua_ls.setup({
+                settings={
+                    Lua={
+                        diagnostics={
+                            globals={
+                                'vim',
+                                'require',
+                            },
+                        },
+                    },
+                },
+            })
+
+            -- PHP
             lspconfig.intelephense.setup({})
+
+            -- JS / TS
             lspconfig.tsserver.setup({})
+
+            -- HTML
             lspconfig.html.setup({})
+
+            -- Python
             lspconfig.pyright.setup({})
+
             -- Use LspAttach autocommand to only map the following keys
             -- after the language server attaches to the current buffer
             vim.api.nvim_create_autocmd('LspAttach', {
@@ -42,8 +64,10 @@ return {
                 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
                 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                --vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                 vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                 -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
                 -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
                 -- vim.keymap.set('n', '<space>wl', function()
@@ -51,12 +75,16 @@ return {
                 -- end, opts)
                 -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
                 -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-                vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
                 -- vim.keymap.set('n', '<space>f', function()
                 --  vim.lsp.buf.format { async = true }
                 -- end, opts)
               end,
+            })
+
+            vim.diagnostic.config({
+                virtual_text = {
+                    severity= { min = vim.diagnostic.severity.WARN },
+                },
             })
         end
     }
