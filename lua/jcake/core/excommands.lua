52e24e4ad45b7db46cd73ce54944299f5a775dc8
Jon Stockton <jonstockton1416@gmail.com>
2024-03-01 08:48:53 -0500

    Add command to switch from 2 spaces to 4 spaces

---
+++ lua/jcake/core/excommands.lua
@@ -0,0 +1,15 @@
+vim.api.nvim_create_user_command(
+    'JCFormat', function(args)
+        local range_spec = '';
+
+        if args.range == 2 then
+            range_spec = args.line1 .. ',' .. args.line2;
+        end
+
+        vim.cmd('set tabstop=2 softtabstop=2 noexpandtab')
+        vim.cmd(range_spec .. 'retab!')
+        vim.cmd('set tabstop=4 softtabstop=4 expandtab')
+        vim.cmd(range_spec .. 'retab')
+    end,
+    { desc = 'Change 2 space to 4 space tabs', range=true }
+)
