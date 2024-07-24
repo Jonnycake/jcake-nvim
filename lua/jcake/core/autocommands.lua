3ea65b353e63395cb10478677b6ac596ca9bd41c
Jonnycake <jonstockton1416@gmail.com>
2024-07-24 09:10:47 -0400

    Clear the jump list when opening vim

--- lua/jcake/core/autocommands.lua
+++ lua/jcake/core/autocommands.lua
@@ -1,10 +1,15 @@
 local commands = {
     'colorscheme habamax',
     'filetype indent off',
+    'clearjumps',
 }
 
 vim.api.nvim_create_autocmd('BufLeave', {
     command="mark e"
+})
+
+vim.api.nvim_create_autocmd('VimEnter', {
+    command="clearjumps"
 })
 
 for _, command in pairs(commands) do
